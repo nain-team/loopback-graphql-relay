f04e45f29e4f1cceb3c756a77ac62a2dec79a8b4
Atif <you@example.com>
2018-01-03 19:10:01 +0500

    server configured

--- src/subscriptions/index.js
+++ src/subscriptions/index.js
@@ -2,22 +2,30 @@ const { createServer } = require('http');
 const { SubscriptionServer } = require('subscriptions-transport-ws');
 const { execute, subscribe } = require('graphql');
 const bodyParser = require('body-parser');
-const { graphiqlExpress } = require('apollo-server-express');
-const graphqlHTTP = require('express-graphql');
+const graphql = require('graphql-server-express');
 
 module.exports = function (app, schema, opts) {
-  app.use('/graphql', bodyParser.json(), graphqlHTTP({
-    schema,
-    rootValue: global,
-    graphiql: true,
-  }));
   const PORT = 3000;
-  app.use('/graphiql', graphiqlExpress({
-    endpointURL: '/graphql',
+  const graphiqlPath = '/graphiql';
+  const path = '/graphql';
+
+  app.use(path, bodyParser.json(), graphql.graphqlExpress(req => ({
+    schema,
+    context: {
+      app,
+      req,
+    },
+  })));
+
+  app.use(graphiqlPath, graphql.graphiqlExpress({
+    endpointURL: path,
     subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`,
+
   }));
 
+
   const server = createServer(app);
+
   server.listen(PORT, () => {
     SubscriptionServer.create(
       { execute, subscribe, schema },
